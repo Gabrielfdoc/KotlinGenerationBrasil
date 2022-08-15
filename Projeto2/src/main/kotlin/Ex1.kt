528dd3fd2a210b78b5018d4608b09266db02fac4
Gabriel Ferreira <gabriel9892@gmail.com>
2022-08-15 18:08:59 -0300

    Otimizados exercicio 1 e 2 do projeto 2 com o uso de funções

--- Projeto2/src/main/kotlin/Ex1.kt
+++ Projeto2/src/main/kotlin/Ex1.kt
@@ -3,19 +3,31 @@ import java.time.LocalDate
 class Ex1 {
     fun ex1() {
         println("Execução do exercicio abaixo:\n\n")
+        val anoAtual = LocalDate.now().year
 
         print("Digite a sua idade: ")
         val idade = readln().toInt()
 
         print("Você já fez aniversário este ano? (s/n): ")
-        val fezAniversario = readln()
+        val fezAniversario = readln().uppercase()
 
-        val anoAtual = LocalDate.now().year
-
-        if (fezAniversario == "s") {
+        if (converteSOuNParaBoolean(fezAniversario)) {
             println("O ano de nascimento é ${anoAtual - idade}")
-        } else if (fezAniversario == "n") {
+        } else {
             println("O ano de nascimento é ${anoAtual - idade - 1}")
         }
+    }
+
+    private fun converteSOuNParaBoolean(sOuN: String): Boolean {
+        val valorRetornado: Boolean
+
+        if (sOuN == "S")
+            valorRetornado = true
+        else if (sOuN == "N")
+            valorRetornado = false
+        else
+            throw IllegalArgumentException()
+
+        return valorRetornado
     }
 }
--- Projeto2/src/main/kotlin/Ex2.kt
+++ Projeto2/src/main/kotlin/Ex2.kt
@@ -5,11 +5,22 @@ class Ex2 {
         println("Execução do exercicio abaixo:\n\n")
 
         print("Digite o diâmetro do círculo: ")
-        val diametro = readln().toDouble()
+        val diametro = readln()
+        val diametroDouble = diametro.replace(",", ".").toDouble()
 
+        println("A area do círculo é ${"%.2f".format(area(diametroDouble))}")
+        print("O perimetro do círculo é ${"%.2f".format(perimetro(diametroDouble))}")
+    }
+
+    private fun area(diametroDouble: Double): Double {
         val pi = 3.14
 
-        println("A area do círculo é ${"%.2f".format(pi * (diametro.pow(2)) / 4)}")
-        print("O perimetro do círculo é ${"%.2f".format(diametro * pi)}")
+        return (pi * (diametroDouble.pow(2)) / 4)
+    }
+
+    private fun perimetro(diametroDouble: Double): Double {
+        val pi = 3.14
+
+        return diametroDouble * pi
     }
 }
